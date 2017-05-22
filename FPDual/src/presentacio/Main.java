57c98f4a0c9b44c4dc7572383e90d87c967f222d
fran <fran@fran-PC.almata.es>
2017-05-22 19:31:11 +0200

    he canviat el main

--- FPDual/src/presentacio/Main.java
+++ FPDual/src/presentacio/Main.java
@@ -44,7 +44,7 @@ public class Main {
 		System.out.println("MENU");
 		System.out.println("1 - Manteniment de centres");
 		System.out.println("2 - Manteniment d'alumnes");
-		System.out.println("3");
+		System.out.println("3 - Manteniment de tutors");
 		System.out.println("4");
 				
 		in.close();

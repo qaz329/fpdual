8a6bc7d58baf0737f1ad369bf42d3bf159812e36
JOAN ESPUNYES <joanespunyes@hotmail.com>
2017-05-23 09:49:19 +0200

    add sergi part

--- FPDual/src/model/Tutor.java
+++ FPDual/src/model/Tutor.java
@@ -1,7 +1,70 @@
 package model;
 
-public class Tutor extends Usuari {
+public class Tutor extends Usuari{
+	int id_usuari;
+	String tecnologia;
+	
+	public Tutor(int id_usuari, String tecnologia) {
+		super();
+		Usuari u=new Usuari();
+		this.id_usuari = u.getId();
+		this.tecnologia = tecnologia;
+	}
+	
+	
+	public Tutor(){
+		super();
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Tutor [id_usuari=" + id_usuari +", tecnologia=" + tecnologia + "]";
+	}
 
-	private int idTutor;
-	private String tecnologia;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + id_usuari;
+		result = prime * result + ((tecnologia == null) ? 0 : tecnologia.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tutor other = (Tutor) obj;
+		if (id_usuari != other.id_usuari)
+			return false;
+		if (tecnologia == null) {
+			if (other.tecnologia != null)
+				return false;
+		} else if (!tecnologia.equals(other.tecnologia))
+			return false;
+		return true;
+	}
+
+
+	public int getId_usuari() {
+		return id_usuari;
+	}
+	public void setId_usuari(int id_usuari) {
+		this.id_usuari = id_usuari;
+	}
+	public String getTecnologia() {
+		return tecnologia;
+	}
+	public void setTecnologia(String tecnologia) {
+		this.tecnologia = tecnologia;
+	}
+	
+	
 }
---
+++ FPDual/src/model/TutorDAO.java
@@ -0,0 +1,54 @@
+package model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import controlador.Constants;
+import controlador.GestorDB;
+
+public class TutorDAO {
+	String consultaSQL;
+	Statement stmt;
+	GestorDB g;
+	
+	public TutorDAO(){
+		g=new GestorDB(Constants.SERVER, Constants.PORT, Constants.BD);
+	}
+	
+	public void altaTutor(Tutor t){
+		consultaSQL="INSERT INTO tutor VALUES(Id_usuari,'"+t.getTecnologia()+"');";
+		g.modificarRegistre(consultaSQL);
+		
+	}
+	
+	public void baixaTutor(int id){
+		try {
+			consultaSQL="DELETE FROM tutor WHERE id_usuari="+id+";";
+			stmt.addBatch(consultaSQL);
+			consultaSQL="DELETE FROM usuari WHERE id_usuari="+id+";";
+			stmt.addBatch(consultaSQL);
+			stmt.executeBatch();
+		} catch (SQLException e) {
+			System.out.println("Error baixa tutor "+e.toString());
+		}
+	}
+	
+	public List<String> consultaTutor(){
+		consultaSQL="SELECT * FROM tutor;";
+		ResultSet rs;
+		List<String> dades = new ArrayList<String>();
+		try {
+			rs=g.consultaRegistres(consultaSQL);
+			while(rs.next()){
+				dades.add(rs.getString("Id_usuari"));
+				dades.add(rs.getString("Tecnologia"));
+			}
+		} catch (SQLException e) {
+			System.out.println("Error consulta tutor "+e.toString());
+		}
+		return dades;
+	}
+}

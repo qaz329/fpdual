643b78f71786f82e55e74a6525f1b641bf6efe83
tehachikyan <tehachikyan@TEHACHYKYAN00W7.indra.es>
2017-05-24 11:46:22 +0200

    Alta i baixa activitats i actualitzat main

---
+++ FPDual/src/model/ActivitatDAO.java
@@ -0,0 +1,49 @@
+package model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import controlador.Constants;
+import controlador.GestorDB;
+
+public class ActivitatDAO {
+	GestorDB gestorDB;
+	
+	public ActivitatDAO(){
+		gestorDB = new GestorDB(Constants.SERVER, Constants.PORT, Constants.BD);
+	}
+	
+	public void altaActivitat(String codi, String desc){
+		String consultaSQL = "INSERT INTO ACTIVITAT(CODI, DESCRIPCIO) VALUES('"+codi+"', '"+desc+"')";
+		if(gestorDB.modificarRegistre(consultaSQL)!=0){
+			System.out.println("Alta realitzada amb èxit");
+		}
+	}
+	
+	public void baixaActivitat(int id){
+		String consultaSQL = "DELETE FROM ACTIVITAT WHERE ID_ACTIVITAT="+id+"";
+		if(gestorDB.modificarRegistre(consultaSQL)!=0){
+			System.out.println("Baixa realitzada amb èxit");
+		}else{
+			System.out.println("No s'ha trobat l'ID");
+		}
+	}
+	
+	public List<Integer> consultarID(){
+		String sentencia = "SELECT id_activitat FROM activitat";
+		ResultSet rs;
+		List<Integer> dades = new ArrayList<Integer>();
+		try{
+			rs = gestorDB.consultaRegistres(sentencia);
+			while(rs.next())
+				dades.add(rs.getInt(1));
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		return dades;
+	}
+	
+	
+}

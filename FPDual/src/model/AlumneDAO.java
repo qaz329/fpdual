90107cb2eee2fb01c12b56d3634d0d77496d7936
JOAN ESPUNYES <joanespunyes@hotmail.com>
2017-05-24 12:26:34 +0200

    dao

--- FPDual/src/model/AlumneDAO.java
+++ FPDual/src/model/AlumneDAO.java
@@ -1,5 +1,10 @@
 package model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import controlador.Constants;
 import controlador.GestorDB;
 
@@ -11,12 +16,58 @@ public class AlumneDAO {
 	}
 	
 	public void altaAlumne(String inici, String fi, int id_tutor, int id_centre, String nif){
-		id_tutor=2;
-		id_centre=1;
+
 		String consultaSQL = "INSERT INTO alumne(id_usuari,data_inici,data_fi,id_centre,id_tutor) "
 				+ "SELECT id_usuari, '"+inici+"','"+fi+"', "+id_centre+", "+id_tutor+" "
 				+ "FROM usuari WHERE NIF LIKE '"+nif+"'";
 		gestorDB.modificarRegistre(consultaSQL);
 		
+	}
+	
+	public void baixaAlumne(int id){
+		String consultaSQL = "DELETE FROM alumne WHERE id_usuari="+id;
+		gestorDB.modificarRegistre(consultaSQL);
+		consultaSQL = "DELETE FROM usuari WHERE id_usuari="+id;
+		gestorDB.modificarRegistre(consultaSQL);
+	}
+	
+	public Object[] consultaAlumnes(){
+		ResultSet rs = null;
+		String consultaSQL = "SELECT u.id_usuari, u.nom, u.primer_cognom, a.data_inici, a.data_fi, c.NOM, a.id_tutor "
+				+ "FROM alumne AS a, usuari AS u, centre AS c "
+				+ "WHERE u.id_usuari=a.id_usuari AND a.id_centre=c.Id_centre";
+		ArrayList<Object> fila = new ArrayList<>();
+		int i = 0;
+		try{
+			rs = gestorDB.consultaRegistres(consultaSQL);
+			while(rs.next()){
+				for(i=0; i<7; i++){
+					fila.add(rs.getObject(i+1));
+				}
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		return fila.toArray();
+	}
+	
+	public List<String> consultaIDAlumne(){
+		String consultaSQL="SELECT u.id_usuari, u.nom, u.primer_cognom FROM usuari AS u, alumne AS a WHERE a.id_usuari=u.id_usuari";
+		ResultSet rs = null;
+		List<String> dades = new ArrayList<String>();
+		int i=0;
+		try {
+			rs = gestorDB.consultaRegistres(consultaSQL);
+			while (rs.next()) {
+				String strTmp = "";
+				strTmp += rs.getInt("id_usuari")+", ";
+				strTmp += rs.getString("nom")+" ";
+				strTmp += rs.getString("primer_cognom");
+				dades.add(strTmp);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return dades;
 	}
 }

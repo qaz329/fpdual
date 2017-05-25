036f2acc87defb5b7672025d6ff35ce6c78d9500
fnavarrov <fnavarrov@FNAVARROV00W7.indra.es>
2017-05-25 10:13:01 +0200

    dddd

--- FPDual/src/presentacio/MantenimentCentres.java
+++ FPDual/src/presentacio/MantenimentCentres.java
@@ -253,7 +253,7 @@ public class MantenimentCentres {
 				} while (!enters);
 
 			} catch (NumberFormatException e) {
-				// TODO Auto-generated catch block
+
 				e.printStackTrace();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block

a70ef0f88b856ccb9286b8e9fe1727c0c0efccdb
JOAN ESPUNYES <joanespunyes@hotmail.com>
2017-05-25 09:14:46 +0200

    alumne

--- FPDual/src/model/Alumne.java
+++ FPDual/src/model/Alumne.java
@@ -7,6 +7,50 @@ public class Alumne extends Usuari {
 	private String dataFi;
 	private Centre centre;
 	private Tutor tutor;
+		
+	public Alumne() {
+		super();
+	}
 	
-	//idcentre idtutor
+	public Alumne(int idAlumne, String dataInici, String dataFi, Centre centre, Tutor tutor) {
+		super();
+		this.idAlumne = idAlumne;
+		this.dataInici = dataInici;
+		this.dataFi = dataFi;
+		this.centre = centre;
+		this.tutor = tutor;
+	}
+	
+	public int getIdAlumne() {
+		return idAlumne;
+	}
+	public void setIdAlumne(int idAlumne) {
+		this.idAlumne = idAlumne;
+	}
+	public String getDataInici() {
+		return dataInici;
+	}
+	public void setDataInici(String dataInici) {
+		this.dataInici = dataInici;
+	}
+	public String getDataFi() {
+		return dataFi;
+	}
+	public void setDataFi(String dataFi) {
+		this.dataFi = dataFi;
+	}
+	public Centre getCentre() {
+		return centre;
+	}
+	public void setCentre(Centre centre) {
+		this.centre = centre;
+	}
+	public Tutor getTutor() {
+		return tutor;
+	}
+	public void setTutor(Tutor tutor) {
+		this.tutor = tutor;
+	}
+	
+	
 }
